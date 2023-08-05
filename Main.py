3b1b4543578ab1b71124b368f1f29892fa3a3c67
David-Ykz <nerdkid314159@gmail.com>
2023-08-05 17:10:34 -0400

    implemented collision detection

--- Main.py
+++ Main.py
@@ -5,8 +5,17 @@ def distance(x1, y1, x2, y2):
     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 def calculateSlope(segment):
     return (segment[3] - segment[1]) / (segment[2] - segment[0])
-def intercept(segment):
+def calculateIntercept(segment):
     return segment[1] - calculateSlope(segment) * segment[0]
+def quadraticFormula(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return []
+    elif discriminant == 0:
+        return [-b / (2 * a)]
+    else:
+        return [(-b + math.sqrt(discriminant)) / (2 * a), (-b - math.sqrt(discriminant)) / (2 * a)]
+
 def drawCircle(color, x, y, r):
     pygame.draw.circle(screen, color, (x, SCREEN_HEIGHT - y), r)
 def drawRectangle(color, x, y, w, h):
@@ -25,12 +34,12 @@ def calculateSegmentIntersection(segment1, segment2):
         return -1
     # First line is vertical
     if segment1[0] == segment1[2]:
-        intersectionPoint = (segment1[0], calculateSlope(segment2) * segment1[0] + intercept(segment2))
+        intersectionPoint = (segment1[0], calculateSlope(segment2) * segment1[0] + calculateIntercept(segment2))
     # Second line is vertical
     elif segment2[0] == segment2[2]:
-        intersectionPoint = (segment2[0], calculateSlope(segment1) * segment2[0] + intercept(segment1))
+        intersectionPoint = (segment2[0], calculateSlope(segment1) * segment2[0] + calculateIntercept(segment1))
     else:
-        intersectionPoint = calculateLineIntersection(calculateSlope(segment1), intercept(segment1), calculateSlope(segment2), intercept(segment2))
+        intersectionPoint = calculateLineIntersection(calculateSlope(segment1), calculateIntercept(segment1), calculateSlope(segment2), calculateIntercept(segment2))
         if intersectionPoint == -1:
             return -1
     lowerBoundX = max(min(segment1[0], segment1[2]), min(segment2[0], segment2[2]))
@@ -41,6 +50,39 @@ def calculateSegmentIntersection(segment1, segment2):
         return intersectionPoint
     else:
         return -1
+def circleLineSegmentIntersection(circleX, circleY, radius, segment):
+    if segment[0] == segment[2]: # Vertical Line
+        if abs(circleX - segment[0]) < radius:
+            yDistance = math.sqrt(radius ** 2 - (circleX - segment[0]) ** 2)
+            if max(segment[1], segment[3]) >= circleY + yDistance >= min(segment[1], segment[3]):
+                return True
+            elif max(segment[1], segment[3]) >= circleY - yDistance >= min(segment[1], segment[3]):
+                return True
+        return False
+
+    lineSlope = calculateSlope(segment)
+    intercept = calculateIntercept(segment)
+    a = 1 + lineSlope ** 2
+    b = -2 * circleX + 2 * lineSlope * (intercept - circleY)
+    c = circleX ** 2 + (intercept - circleY) ** 2 - radius ** 2
+    pointsOfIntersection = quadraticFormula(a, b, c)
+    if len(pointsOfIntersection) == 0:
+        return False
+    for eachIntersection in pointsOfIntersection:
+        if max(segment[0], segment[2]) >= eachIntersection >= min(segment[0], segment[2]):
+            return True
+    return False
+def checkCollisions(circleX, circleY, radius, allObstacles):
+    anyCollision = False
+    for eachObstacle in allObstacles:
+        for i in range(len(eachObstacle) - 1):
+            segment = [eachObstacle[i][0], eachObstacle[i][1], eachObstacle[i+1][0], eachObstacle[i+1][1]]
+            if circleLineSegmentIntersection(circleX, circleY, radius, segment):
+                anyCollision = True
+    return anyCollision
+
+
+
 
 # pygame setup
 pygame.init()
@@ -50,6 +92,7 @@ SIDEBAR_LENGTH = 200
 heading = math.pi/2
 xPos = SCREEN_WIDTH/2
 yPos = SCREEN_HEIGHT/2
+roombaRadius = 20
 screen = pygame.display.set_mode((SCREEN_WIDTH + SIDEBAR_LENGTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
@@ -73,13 +116,19 @@ while running:
     if pygame.key.get_pressed()[pygame.K_UP]:
         xPos += math.cos(heading)
         yPos += math.sin(heading)
+        if checkCollisions(xPos, yPos, roombaRadius, obstaclePositions):
+            xPos -= math.cos(heading)
+            yPos -= math.sin(heading)
     elif pygame.key.get_pressed()[pygame.K_DOWN]:
         xPos -= math.cos(heading)
         yPos -= math.sin(heading)
+        if checkCollisions(xPos, yPos, roombaRadius, obstaclePositions):
+            xPos += math.cos(heading)
+            yPos += math.sin(heading)
 
     screen.fill((255, 255, 255))
     drawRectangle((208, 225, 212), SCREEN_WIDTH, SCREEN_HEIGHT, SIDEBAR_LENGTH, SCREEN_HEIGHT)
-    drawCircle((0, 0, 0), xPos, yPos, 20)
+    drawCircle((0, 0, 0), xPos, yPos, roombaRadius)
     drawLine((100, 100, 255), xPos, yPos, xPos + 60 * math.cos(heading), yPos + 60 * math.sin(heading))
 
 
@@ -101,8 +150,8 @@ while running:
         for i in range(len(eachShape) - 1):
             drawLine((0, 0, 0), eachShape[i][0], eachShape[i][1], eachShape[i+1][0], eachShape[i+1][1])
             segment2 = [eachShape[i][0], eachShape[i][1], eachShape[i+1][0], eachShape[i+1][1]]
-            drawCircle((0, 255, 0), segment2[0], segment2[1], 5)
-            drawCircle((0, 255, 0), segment2[2], segment2[3], 5)
+            if circleLineSegmentIntersection(xPos, yPos, roombaRadius, segment2):
+                print("True")
             intersectionPoint = calculateSegmentIntersection(segment1, segment2)
             if intersectionPoint != -1:
                 intersections.append(intersectionPoint)
